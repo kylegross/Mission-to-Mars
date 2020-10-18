e5121215821af153427731acf10c1e1b50bb7851
Gross <kgross@asg-architects.com>
2020-10-18 16:36:06 -0400

    It's finally workinggit add .

--- scraping.py
+++ scraping.py
@@ -10,12 +10,12 @@ def scrape_all():
     executable_path = {'executable_path': 'chromedriver.exe'}
     browser = Browser('chrome', **executable_path, headless=True)
 
-    news_title, news_paragraph = mars_news(browser)
+    news_title, news_p = mars_news(browser)
 
     # Run all scraping functions and store results in a dictionary
     data = {
         'news_title': news_title,
-        'news_paragraph': news_paragraph,
+        'news_paragraph': news_p,
         'featured_image': featured_image(browser),
         'facts': mars_facts(),
         'last_modified': dt.datetime.now()
@@ -24,7 +24,6 @@ def scrape_all():
     # Stop webdriver and return data
     browser.quit()
     return data
-
 
 def mars_news(browser):
 
